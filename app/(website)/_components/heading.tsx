79b65b1296052e54779264515fdd193b6fe2c8e9
hesham-04 <hesham.fractured@gmail.com>
2025-10-25 18:04:21 +0500

    Website portion login using clerk

--- app/(website)/_components/heading.tsx
+++ app/(website)/_components/heading.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import  Link from 'next/link';
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+
+    const { isAuthenticated, isLoading } = useConvexAuth()
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,9 +21,27 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Trojan is a connected and integrated workspace for developers.
             </h3>
-            <Button>
-                Get Started <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            { isLoading && (
+                <div className='w-full flex items-center justify-center'>
+                    <Spinner />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                    <Button asChild>
+                        <Link href='/documents'>
+                            Enter Trojan
+                            <ArrowRight className="h-4 w-4 ml-2" />
+                        </Link>
+                    </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Started for Free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
 
         </div>
     );
