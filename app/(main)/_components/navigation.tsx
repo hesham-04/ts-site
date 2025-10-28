9d13539d86ea059a43049e547e7ab8ba6d2bf171
hesham-04 <hesham.fractured@gmail.com>
2025-10-28 17:53:55 +0500

    funcs assigned

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -58,10 +58,23 @@ export const Navigation = () => {
         }
     }
 
+    const collapse = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(true);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = `0px`;
+            navbarRef.current.style.setProperty("width", `100%`);
+            navbarRef.current.style.setProperty("left", `0px`);
+            setTimeout(() => setIsResetting(false), 300);
+
+        }
+    }
+
 return (
     <>
         <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999] pointer-events-auto", isResetting && "transition-all duration-300 ease-in-out", isMobile && "w-0 pointer-events-none")}>
-            <div role="button" className={cn("absolute top-3 right-2 h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-700 opacity-0 group-hover/sidebar:opacity-100 transition", isMobile && "opacity-100")}>
+            <div onClick={collapse} role="button" className={cn("absolute top-3 right-2 h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-700 opacity-0 group-hover/sidebar:opacity-100 transition", isMobile && "opacity-100")}>
                   <ChevronsLeft className="h-6 w-6" /> {isMobile}
             </div>
             <div>
@@ -75,7 +88,7 @@ return (
 
     <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all duration-300 ease-in-out", isMobile && "left-0 w-full")}>
         <nav className="bg-transparent px-3 py-2 w-full">
-            {isCollapsed && <MenuIcon role='button' className='h-6 2-6 text-muted-foreground' />}
+            {isCollapsed && <MenuIcon onClick={restWidth} role='button' className='h-6 2-6 text-muted-foreground' />}
         </nav>
     </div>
     </>
