fd81c911a87da420cedb8b18df926e3e38ec5580
hesham-04 <hesham.fractured@gmail.com>
2025-10-28 17:40:48 +0500

    NAV: resetWidthHandle

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -45,6 +45,19 @@ export const Navigation = () => {
         document.removeEventListener("mouseup", handleMouseUp);
     }
 
+    const restWidth = () => {
+        if (sidebarRef.current && navbarRef.current) {
+            setIsCollapsed(false);
+            setIsResetting(true);
+
+            sidebarRef.current.style.width = isMobile ? `100%` : `240px`;
+            navbarRef.current.style.setProperty("width", isMobile ? `0px` : `100% - 240px`);
+            navbarRef.current.style.setProperty("left", isMobile ? `100px` : `240px`);
+            setTimeout(() => setIsResetting(false), 300);
+
+        }
+    }
+
 return (
     <>
         <aside ref={sidebarRef} className={cn("group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999] pointer-events-auto", isResetting && "transition-all duration-300 ease-in-out", isMobile && "w-0 pointer-events-none")}>
@@ -57,7 +70,7 @@ return (
             <div className="mt-4">
                 <p>Documents</p>
             </div>
-            <div onMouseDown={handleMouseDown} onClick={() => {}} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+            <div onMouseDown={handleMouseDown} onClick={restWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
         </aside>
 
     <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", isResetting && "transition-all duration-300 ease-in-out", isMobile && "left-0 w-full")}>
