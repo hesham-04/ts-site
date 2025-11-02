11bde2c7bb7fcce3d32ea78cd9c881f409fe6cd9
hesham-04 <hesham.fractured@gmail.com>
2025-11-02 17:39:14 +0500

    Convex is goated fr

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,24 @@ import Image from 'next/image'
 import {useUser} from '@clerk/clerk-react'
 import {Button} from '@/components/ui/button';
 import {PlusCircleIcon} from "lucide-react";
+import {useMutation} from "convex/react";
+import {api} from "@/convex/_generated/api"
+
+import {toast} from "sonner"
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: 'Untitled Document' });
+
+        toast.promise(promise, {
+            loading: 'Creating document...',
+            success: 'Document created!',
+            error: 'Failed to create document.'
+        })
+    }
     return (
         <div className='h-full flex flex-col items-center justify-center space-y-4'>
             <Image src='/enter-light.png' height={300} width={300} alt='enter' priority quality={80} className='dark:hidden ' />
@@ -15,7 +30,7 @@ const DocumentsPage = () => {
                 Welcome to {user?.firstName}&apos;s Trojan!
             </h2>
 
-            <Button variant='default' className='ml-4'>
+            <Button variant='default' className='ml-4' onClick={onCreate}>
                 <PlusCircleIcon className='h-4 w-4 mr-2' />
                 Create a note
             </Button>
--- convex/documents.ts
+++ convex/documents.ts
@@ -21,5 +21,4 @@ export const create = mutation({
             userId:userId,
         });
     }
-
-});
+                });
