18ff75a1c2250dc84d3488604941c515a7f0aed4
hesham-04 <hesham.fractured@gmail.com>
2025-11-09 18:24:50 +0500

    Item archive ui

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import {ChevronDown, ChevronRight, LucideIcon, PlusIcon} from "lucide-react";
+import {TrashIcon, ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, PlusIcon} from "lucide-react";
 import {Id} from "@/convex/_generated/dataModel";
 import {cn} from "@/lib/utils";
 import {Skeleton} from "@/components/ui/skeleton";
 import {useMutation} from "convex/react";
 import {api} from "@/convex/_generated/api"
 import {useRouter} from "next/navigation";
+import {useUser} from "@clerk/clerk-react";
 import {toast} from "sonner";
 import React from "react";
+import {DropdownMenu, DropdownMenuTrigger, DropdownMenuSeparator, DropdownMenuContent, DropdownMenuItem} from "@/components/ui/dropdown-menu";
 
 interface ItemProps {
     id?: Id<'documents'>;
@@ -26,6 +28,7 @@ interface ItemProps {
 export const Item = ({id, active, docmuentIcon, isSearch, level=0, onExpand, expanded, label, onClick, icon:Icon}:ItemProps) => {
     const ChevronIcon = expanded ? ChevronDown :ChevronRight
     const create = useMutation(api.documents.create);
+    const {user} = useUser();
     const router = useRouter();
 
     const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
@@ -74,6 +77,25 @@ export const Item = ({id, active, docmuentIcon, isSearch, level=0, onExpand, exp
             )}
             {!!id && (
                 <div className='ml-auto flex items-center gap-x-2'>
+                    {/*The dropdown menu*/}
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+                            <div role='button' className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'>
+                                <MoreHorizontal className='h-4 w-4 text-muted-foreground'/>
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className='w-60' align='start' side='right' forceMount>
+                            <DropdownMenuItem onClick={() => {}}>
+                                <TrashIcon className='h-4 w-4 mr-2 text-red-500'/> Delete
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <div className='text-xs text-muted-foreground p-2'>
+                                Last edited by {user?.firstName} { user?.emailAddresses[0]?.emailAddress}
+                            </div>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    {/*The add child plus + button*/}
                     <div className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600' role='button' onClick={onCreate}>
                         <PlusIcon className='h-4 w-4 text-muted-foreground '/>
                     </div>
